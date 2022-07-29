7186c6abdb363980cb3216997d4d8068aa1595f0
Vadim Meshcheryakov <painassasin@icloud.com>
2022-07-29 13:06:11 +0500

    111

--- project/dao/__init__.py
+++ project/dao/__init__.py
@@ -1,5 +1,11 @@
-from .main import GenresDAO
+from .auth_dao import AuthDAO
+from .main_dao import GenresDAO, DirectorsDAO, MoviesDAO
+from .users_dao import UsersDAO
 
 __all__ = [
     'GenresDAO',
+    'DirectorsDAO',
+    'MoviesDAO',
+    'UsersDAO',
+    'AuthDAO'
 ]
--- project/setup/api/models.py
+++ project/setup/api/models.py
@@ -6,3 +6,38 @@ genre: Model = api.model('Жанр', {
     'id': fields.Integer(required=True, example=1),
     'name': fields.String(required=True, max_length=100, example='Комедия'),
 })
+
+director: Model = api.model('Режисер', {
+    'id': fields.Integer(required=True, example=1),
+    'name': fields.String(required=True, max_length=100, example='Кристофер Нолан'),
+})
+
+movie: Model = api.model('Фильм', {
+    'id': fields.Integer(required=True, example=1),
+    'title': fields.String(required=True, max_length=100, example='Интерстллар'),
+    'description': fields.String(),
+    'trailer': fields.String(),
+    'year': fields.String(),
+    'rating': fields.String(),
+    'genre': fields.Nested(genre),
+    'director': fields.Nested(director),
+})
+
+user: Model = api.model('Пользователь', {
+    'id': fields.Integer(required=True, example=1),
+    'email': fields.String(required=True, max_length=100, example='tratata@mail.ru'),
+    'password': fields.String(),
+    'name': fields.String(),
+    'surname': fields.String(),
+    'genre': fields.Nested(genre)
+})
+
+auth: Model = api.model('Регистрация', {
+    'email': fields.String(required=True, max_length=100, example='tratata@mail.ru'),
+    'password': fields.String(),
+})
+
+tokens: Model = api.model('Токены', {
+    'access_token': fields.String(),
+    'refresh_token': fields.String(),
+})
